4cdbdc75a44651ec0d3359c73333d795aeba08b6
NoobAuthor <igor21.assis@outlook.com>
2024-08-08 19:40:56 -0400

    Step 2

--- main.go
+++ main.go
@@ -13,11 +13,63 @@ type Cell struct {
 	IsMine        bool
 	IsReavealed   bool
 	IsFlagged     bool
-	AdjecentMines int
+	AdjacentMines int
 }
 
 type Board struct {
 	Rows  int
 	Cols  int
 	Cells [][]Cell
+}
+
+func NewBoard(rows, cols, mines int) *Board {
+	board := &Board{
+		Rows:  rows,
+		Cols:  cols,
+		Cells: make([][]Cell, rows),
+	}
+
+	for i := range board.Cells {
+		board.Cells[i] = make([]Cell, cols)
+	}
+
+	placeMines(board, mines)
+	calculateAdjacentMines(board)
+
+	return board
+}
+
+func placeMines(board *Board, mines int) {
+	rand.Seed(time.Now().UnixNano())
+	for mines > 0 {
+		r := rand.Intn(board.Rows)
+		c := rand.Intn(board.Cols)
+		if !board.Cells[r][c].IsMine {
+			board.Cells[r][c].IsMine = true
+			mines--
+		}
+	}
+}
+
+func calculateAdjacentMines(board *Board) {
+	directions := []struct{ dr, dc int }{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	for r := 0; r < board.Rows; r++ {
+		for c := 0; c < board.Cols; c++ {
+			if board.Cells[r][c].IsMine {
+				continue
+			}
+
+			for _, d := range directions {
+				nr, nc := r+d.dr, c+d.dc
+				if nr >= 0 && nr < board.Rows && nc >= 0 && nc < board.Cols && board.Cells[nr][nc].IsMine {
+					board.Cells[r][c].AdjacentMines++
+				}
+			}
+		}
+	}
 }
